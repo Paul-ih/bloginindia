49311b6fed92987d7798d79ed1e08acb56cb4de5
Mackenzie27 <madameminch@gmail.com>
2021-03-12 09:28:09 +0100

    trying to get things online

--- config/session.config.js
+++ config/session.config.js
@@ -15,7 +15,7 @@ module.exports = app => {
     // use session
     app.use(
       session({
-        secret: process.env.SESS_SECRET,
+        secret: process.env.SESSION_SECRET,
         resave: true,
         saveUninitialized: true,
         cookie: {
